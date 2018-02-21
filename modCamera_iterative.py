dd0f44e082bb18e9e973f1b9bc614b315c28b3d3
Markus Trockel <post@markustrockel.de>
2018-02-21 15:57:16 +0100

    modCam_iterative error correction

--- modCamera_iterative.py
+++ modCamera_iterative.py
@@ -116,9 +116,9 @@ def process_lines(frame):
     right_line_list = []
     for line in sorted_list:
         # show all detected line infos:
-        if abs(line[1] - prev_line_left) < DIV_2_PREV:
+        if abs(line[1] - prev_line_left[1]) < DIV_2_PREV:
             left_line_list.append(line)
-        if abs(line[1] - prev_line_right) < DIV_2_PREV:
+        if abs(line[1] - prev_line_right[1]) < DIV_2_PREV:
             right_line_list.append(line)
 
     # calculate mean-lines LEFT and RIGHT
